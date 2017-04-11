c1e6b5ab3af142e18c66d0437f157cbac4b7dcec
Larisa Carli <lara.carli@gmail.com>
2017-04-11 11:41:12 +0200

    first upload01

--- kviz.py
+++ kviz.py
@@ -3,10 +3,27 @@ from tkinter import*
 class Kviz():
 
     def __init__(self, master):
-        self.platno = Canvas(master, width=1000, height=800)
-        self.platno.pack()
+        self.prikaziZacetnoOkno()
+
+
+    def prikaziZacetnoOkno(self):
+        top_frame = Frame(root)
+        top_frame.pack()
+        text = Text(top_frame, width = 40, height=2, spacing1 = 15)
+        text.insert(INSERT, "Izberi področje:")
+
+        text.config(state=DISABLED)
+        text.pack()
+
+        buttonGeo = Button(top_frame, text="Geografija", command=prikaziGeografijo).pack(fill=X)
+        buttonMat = Button(top_frame, text="Matematika").pack(fill=X)
+
+    def prikaziGeografijo(self):
+
+
 
 root = Tk()
 root.title('Kviz')
 app = Kviz(root)
 root.mainloop()
+
